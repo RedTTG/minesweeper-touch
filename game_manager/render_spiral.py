feb0cb83d20de75e4f7855cd2a7735533302fb9b
red <red@redttg.com>
2023-07-31 12:56:17 +0300

    circle!!

--- game_manager/render_spiral.py
+++ game_manager/render_spiral.py
@@ -5,32 +5,14 @@ def render_spiral(start_x, start_y, end_x, end_y, max_chunks_to_render):
     mid_x = (start_x + end_x) // 2
     mid_y = (start_y + end_y) // 2
 
-    direction = 1  # 0: right, 1: down, 2: left, 3: up
-    distance = 1
-    step_count = 0
+    points = []
+    for y in range(start_y, end_y):
+        for x in range(start_x, end_x):
+            distance = math.sqrt((x - mid_x) ** 2 + (y - mid_y) ** 2)
+            points.append((x, y, distance))
 
-    x, y = mid_x, mid_y
+    points.sort(key=lambda p: p[2])  # Sort points by distance from center
 
-    max_chunks_to_render = int(math.ceil(math.sqrt(max_chunks_to_render))) ** 2
-
-    for _ in range(max_chunks_to_render):
-        if start_x <= x < end_x and start_y <= y < end_y:
-            yield x, y
-
-        if step_count == distance:
-            step_count = 0
-            if direction in [0, 2]:
-                distance += 1
-
-            direction = (direction + 1) % 4
-
-        if direction == 0:
-            x += 1
-        elif direction == 1:
-            y += 1
-        elif direction == 2:
-            x -= 1
-        elif direction == 3:
-            y -= 1
-
-        step_count += 1
+    for i in range(min(max_chunks_to_render, len(points))):
+        x, y, _ = points[i]
+        yield x, y
