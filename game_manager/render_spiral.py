136d5a395b7c797932ba2da3152bdf116ba1b985
red <red@redttg.com>
2023-07-31 13:33:36 +0300

    hmmm

--- game_manager/render_spiral.py
+++ game_manager/render_spiral.py
@@ -2,19 +2,20 @@ import math
 import heapq
 
 
-def render_spiral(start_x, start_y, end_x, end_y, max_chunks_to_render, reversed=False, min_distance=0):
+def render_spiral(start_x, start_y, end_x, end_y, max_chunks_to_render):
     mid_x = (start_x + end_x) / 2
     mid_y = (start_y + end_y) / 2
 
     def distance_from_center(x, y):
-        return math.sqrt((x - mid_x) ** 2 + (y - mid_y) ** 2)
+        # Calculate the distance of each point from the center along both x and y axes separately
+        x_distance = abs(x - mid_x)
+        y_distance = abs(y - mid_y)
+        return max(x_distance, y_distance)
 
     points_heap = []
     for y in range(start_y, end_y):
         for x in range(start_x, end_x):
             distance = distance_from_center(x, y)
-            if reversed and distance < -min_distance:
-                continue
             if len(points_heap) < max_chunks_to_render:
                 heapq.heappush(points_heap, (-distance, x, y))
             else:
